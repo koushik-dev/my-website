45591212ad0a57c0cd2017ff1a9eb83a3d54a5a3
koushik <koushik.ramaswamy@apitive.com>
2020-04-10 12:31:32 +0530

    project base

---
+++ src/components/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.scss';
+
+function Header() {
+    return (
+        <h1>Opening Soon<span data-top="10">.</span><span data-top="20">.</span><span data-top="30">.</span></h1>
+    )
+}
+
+export default Header;
