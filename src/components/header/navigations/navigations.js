e9b37fdc75ef5228341e58a9768823fb2e5c618c
koushik <koushik.ramaswamy@apitive.com>
2020-04-16 17:26:58 +0530

    gitignore changes

--- src/components/header/navigations/navigations.js
+++ src/components/header/navigations/navigations.js
@@ -6,7 +6,7 @@ function Navigation(props) {
      <div className="navigation">
          <ul className={"md:grid md:grid-cols-3 text-center"}>
              <li className={"col-span-1" + props.isVertical ? "my-5" : "my-0"}><a href="/articles" className="">Articles</a></li>
-             <li className={"col-span-1" + props.isVertical ? "my-5" : "my-0"}><a href="#" className="">Products</a></li>
+             <li className={"col-span-1" + props.isVertical ? "my-5" : "my-0"}><a href="!#" className="">Products</a></li>
              <li className={"col-span-1" + props.isVertical ? "my-5" : "my-0"}><a href="/contact" className="">Contact</a></li>
          </ul>
      </div>   
