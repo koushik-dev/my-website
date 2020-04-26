21982db066cfed7581ae519fba1c0682f68eb32c
koushik <koushik.ramaswamy@apitive.com>
2020-04-26 22:30:05 +0530

    removed react

--- src/pages/articles-page.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function ArticlesPage() {
-    return (
-        <div className="font-black text-xl text-center">ArticlesPage</div>
-    )
-}
-export default ArticlesPage;
--- src/pages/contact-page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ContactPage() {
-    return (
-        <div className="font-black text-xl text-center">ContactPage</div>
-    )
-}
-
-export default ContactPage;
