06cb4d5fd49ebaa833f29d9d213f5601750eb63b
koushik <koushik.ramaswamy@apitive.com>
2020-05-15 22:36:44 +0530

    emotion added

---
+++ src/components/blogs/styledcomp.md
@@ -0,0 +1,8 @@
+---
+title: "Typography and Alignments"
+author: "Koushik"
+date: "12-05-2020"
+path: "/blog/typographyandalignments"
+bannerImg: "./images/typography/banner.jpg"
+---
+![image](./images/typography/banner.jpg)  
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,26 @@
-import styles from "./header.module.css"
+
 import PropTypes from "prop-types"
 import React from "react"
 
 import { Link } from 'gatsby'
+import styled from '@emotion/styled'
+
+const HeaderTag = styled.header`
+    background-color: white;
+    height: 70px;
+    box-shadow: 0 4px 5px var(--shadow-color);
+    color: #000;
+    padding: 10px 20px;
+    a {
+      text-decoration: none;
+      color: var(--text-color);
+    }
+`
 
 const Header = ({ siteTitle }) => (
-  <header className={styles.header}>
+  <HeaderTag>
     <Link to="/"><h1>{siteTitle}</h1></Link>
-  </header>
+  </HeaderTag>
 )
 
 Header.propTypes = {
